43d51d0a1b39988c8747e2ec77f06daa3724f9b0
Guakocius <engelhardt.alexander@mein.gmx>
2024-12-01 21:25:50 +0100

    Removed unused imports

--- src/Day1.java
+++ src/Day1.java
@@ -1,10 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Day1 {
