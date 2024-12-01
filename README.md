bbdf0c1328806e52c20f5faed75d5380224465d6
Guakocius <engelhardt.alexander@mein.gmx>
2024-12-01 21:19:53 +0100

    Completed Day 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent of Code 2024
+## This is a public repository where I will push Advent of Code solutions everyday.
---
+++ src/Day1.java
@@ -0,0 +1,60 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Day1 {
+    private Day1() {}
+
+    public static void main(String[] args) {
+        try {
+            Scanner fileInput = new Scanner(new File("src/inputs.txt"));
+
+            List<Integer> inputList1 = new ArrayList<>();
+            List<Integer> inputList2 = new ArrayList<>();
+
+
+            while(fileInput.hasNextLine()) {{
+                    int data1 = fileInput.nextInt();
+                    int data2 = fileInput.nextInt();
+                    inputList1.add(data1);
+                    inputList2.add(data2);
+                    if(fileInput.hasNextLine()) {
+                        fileInput.nextLine();
+                    }
+                }
+            }
+            fileInput.close();
+            inputList1.sort(Integer::compareTo);
+            inputList2.sort(Integer::compareTo);
+            int addDiff = 0;
+
+            for(int i = 0; i < inputList1.size(); i++) {
+                int diff = Math.abs(inputList1.get(i) - inputList2.get(i));
+                addDiff += diff;
+                }
+
+            Map<Integer, Integer> simil = new HashMap<>();
+            for(int num : inputList2) {
+                simil.put(num, simil.getOrDefault(num, 0) + 1);
+            }
+            int sum = 0;
+            for(int num : inputList1) {
+                int freq = simil.getOrDefault(num, 0);
+                int prod = num * freq;
+                sum += prod;
+            }
+            System.out.println(sum);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+
+
+    }
+}
